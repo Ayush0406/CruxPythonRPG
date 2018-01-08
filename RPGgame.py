f9a34047cf668067dc8c9179c1641bff5c493307
Ayush0406 <34995195+Ayush0406@users.noreply.github.com>
2018-01-08 19:31:49 +0530

    Day 2

--- RPGgame.py
+++ RPGgame.py
@@ -5,9 +5,7 @@ class Character:
 		self.name = name
 		
 	def attack(self, enemy):
-		ememy.health -= self.damage
-		if enemy.health <= 0:
-			print "%r KILLED", enemy.name
+		ememy.take_damage(self.damage)
 			
 	def take_damage(self, damage):
 		self.health -= damage
@@ -18,17 +16,41 @@ class Hero(Character):
 	def __init__(self, damage = 10, health = 30, name = "Hero"):
 		super(Hero, self).__init__(damage, health, name)
 		max_health = 50
+		self.xp = 0
+		self.level = 1
+		self.xplevels = [10, 20, 30, 40, 50 ,60]
+		xp_to_next_level = xplevels[level-1]
+		
 	
 	def rest(self):
-		self.health += self.damage
+		self.health += self.damage 
 		if self.health >= max_health:
 			self.health = max_health
 			print (self.name + ' health full')
 	
-	
+	def level_up(self):
+		self.xp += 10
+		if self.xp is self.xp_to_next_level:
+			self.level += 1
+			print "Congrats %s your level has increased to level %s" %(self.name, self.level)
+			self.xp_to_next_level = self.xplevels[level-1]
+			self.xp = 0
+			
+	def attack(self, enemy):
+		super(Hero, self).attack(enemy)
+		if enemy.health is 0:
+			self.lvlup()
+			
+	def __str__(self):
+		return 'Name: %s \n Health: %s \n Level: %s ' 
+			
+			
 class Monster(Character):
 	def __init__(self, damage, health, name):
 		super(Monster, self).__init__(damage, health, name)
+		
+	def __str__(self):
+		return "Name: %s \n Health: %s" %(sel.name, self.health)
 
 
 class Goblin(Monster):
@@ -40,3 +62,6 @@ class Orc(Monster):
 	def __init__(self, damage = 10, health = 20, name = 'Orc'):
 		super(Orc, self).__init__(damage, health, name)
 	
+def explore():
+	monstertype = random.choices([Goblin(), Orc(), None], [0.4, 0.1, 0.5], k = 1)
+	return monstertype[0]
